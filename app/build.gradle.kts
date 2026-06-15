103e70a3ca9c3d095b3fec2192d6d5e26b47f229
urbanescavenger <urbanescavenger@gmailcom>
2026-06-15 22:08:16 +0800

    Bump to 1.0.5-alpha.3 (versionCode 1005003)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
     applicationId = "com.kirin.mt"
     minSdk = 23
     targetSdk = 36
-    versionCode = 1005002
-    versionName = "1.0.5-alpha.2"
+    versionCode = 1005003
+    versionName = "1.0.5-alpha.3"
 
     ndk {
       abiFilters.clear()
