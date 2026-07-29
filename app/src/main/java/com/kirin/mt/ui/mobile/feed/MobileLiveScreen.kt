fc51f774f57e86a9602f20382be34c63be01d138
urbanescavenger <urbanescavenger@gmailcom>
2026-07-29 09:43:11 +0800

    fix(live): import LazyGridState in MobileLiveScreen

--- app/src/main/java/com/kirin/mt/ui/mobile/feed/MobileLiveScreen.kt
+++ app/src/main/java/com/kirin/mt/ui/mobile/feed/MobileLiveScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridState
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
